f459aa7c077d21614e0de9c253d1f8cc8bd4cf17
FearFree <fearfree@cubetown.net>
2012-09-26 17:26:37 -0500

    New file input.php, added call to new file in input.sh
    New file input.php to sort and not overwrite files.
    Added call to use new file in input.sh, and changed output of split
    command

---
+++ Scripts/input.php
@@ -0,0 +1,20 @@
+<?php
+$prefix="Hashes_"; //Set prefix for files
+$n="a";  //Set starting value for suffix
+$fn="$prefix$n";  //Ending filename is prefix+suffix
+$list=scandir('/home/gravypod/test/output'); //Get names of files into an array
+foreach ($list as $filename) { //Loop for each file
+  $fn="$prefix$n"; //Get new ending filename, as it may change later in loop
+#  echo $fn; //Testing code only
+  if((substr($filename, 0, 17))=="Hashes_pre_entry_") { //Find files that have not yet been enetered into MySQL
+    echo "Found hash file $filename...\n"; //Report each filename found
+  while((file_exists($fn))=="TRUE") {  //Loop to not overwrite existing files
+    echo "Found existing file $fn, skipping...\n"; //Report existing filenames found
+    $n = ++$n; //Advance suffix one character
+    $fn="$prefix$n"; //Reset filename for this loop
+    }
+  shell_exec("mv $filename $fn"); //Rename not yet entered filename to entered filename
+  echo "Renaming $filename to $fn\n"; //Report renaming activity
+  }
+}
+?>
--- Scripts/input.sh
+++ Scripts/input.sh
@@ -1,22 +1,21 @@
 #/bin/bash
-cd /home/gravypod/test
 
-#split raw output into manageable files
-split --verbose -l 1000000 HashList.txt output/Hashes_4
+read -p "File to read? (Use full path): " input
+
+cd /home/gravypod
+split --verbose -a 5 -l 1000000 $input /home/gravypod/test/output/Hashes_pre_entry_
+
+php input.php
 
 cd /home/gravypod/test/output
-#read new files into array
 declare -a files=`ls Hashes_*`
 
-#loop the array
 for file in $files
 do
-echo [`date +%H:%M`] Starting $file...
+echo [`date +%H:%M:%S`] Starting $file...
 
-#create MySQL Tables
   mysql -u mysql_user -p'mysql_pass' rainbowtables -e "CREATE TABLE IF NOT EXISTS $file (pass VARCHAR(300), hash VARCHAR(40), UNIQUE (hash))";
 
-#Import MySQL Data
   mysqlimport -u mysql_user -p"mysql_pass" rainbowtables $file --fields-terminated-by=' ' --lines-terminated-by='\n' --local
 
 cat > $file << EOF
